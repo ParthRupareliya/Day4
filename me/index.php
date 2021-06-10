3c4049db6d8ee32e5df21e07797e25aeef018f20
ParthRupareliya <85487931+ParthRupareliya@users.noreply.github.com>
2021-06-10 16:26:41 +0530

    Add files via upload

---
+++ me/index.php
@@ -0,0 +1,80 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Parth Rupareliya</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+
+
+
+
+
+</head>
+<body>
+
+<div id="header-wrapper">
+	<div id="header">
+		<div id="logo">
+			<h1 style="text-transform: capitalize;">Parth Rupareliya</h1>
+			<p style="font-size: 18px;"> Web-Designer /  web-Developer </a></p>
+		</div>
+	</div>
+</div>
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+		<div><img src="images/me.jpg" width="920" height="900" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title">Welcome &nbsp; to &nbsp; My &nbsp; Page</h2>
+				<p class="meta" style="font-size: 16px;"><span class="date">June 10, 2021</span><span class="posted">Posted by Parth Rupareliya</a></span></p>
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+					<p style="font-size: 28px;">I am web-app developer. I am experienced in PHP projects. I also have grip on laravel, mysql, firebase. I completed many start up projects. I am provide best solution for your problem. i am integrate your selected templete in php. i am providing the best suggestions for relating your project & for advertise your project.  
+					</p>
+				</div>
+			</div></div>
+			
+		<!-- end #content -->
+		<div id="sidebar">
+			<ul>
+				<li>
+					<h2>Services</h2>
+					<ul style="font-size: 16px;">
+						<li><a href="#">Web-Development</a></li>
+						<li><a href="#">Template-Integration</a></li>
+						<li><a href="#">Marketing</a></li>
+						<li><a href="#">Domain/Hosting</a></li>
+						<li><a href="#">Custom-Design</a></li>
+						<li><a href="#">Logo-Making</a></li>
+					</ul>
+				</li>
+				<li>
+					<img style="" src="images/contact.jpg">
+					<ul style="font-size: 17px;">
+						
+						<li>Junagad(362001), Gujrat</li>
+						<li>Email: parthhrupareliya@gmail.com</li>
+						<li>Contact: 9664780136</li>
+						<li><a href="https://www.facebook.com/parth.rupareliya.12"><img style="height: 60px;" src="images/facebook.png" ></a><a href="https://www.instagram.com/blessed_programmer_/"><img style="height: 60px;" src="images/insta.png" ></a></li></li>
+											
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<div id="footer">
+	<p>copyright© 2021.Parth Rupareliya. All rights reserved.</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
